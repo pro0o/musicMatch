4d3099735cad9b391425f9d95eab201ccad77bdd
lavde <104296090+pro0o@users.noreply.github.com>
2022-11-05 23:52:25 +0545

    Update spotifyapi_bruv.cpp

--- spotifyapi_bruv.cpp
+++ spotifyapi_bruv.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 string parseName(string trackName, string artistName) {
+    //replace whitespace with '+'.
     string _temp;
     for (auto x : trackName) {
         if (x == ' ')_temp+='+';
@@ -19,10 +20,11 @@ string parseName(string trackName, string artistName) {
 }
 
 void getTrackID(string trackName, string artistName) {
-    
+    //spotify api link
     string URI = "https://api.spotify.com/v1/search?q=";
     URI += parseName(trackName, artistName);
     URI += "&type=track&limit=1";
+    //authorization token from postman
     string TOKEN = "BQC-SquZA1KsdNI1rAwrS7MLFUXgP-ACL-gbUee0-EzI88Fd0Zd_IAmc62OkW73RctHcdGDJedR13ZhlIo_nZMsn80LFxMrzRdBk01b82BXk-N21UThPvGcY2uxCTiD2oucgkkg_8ltleo4PDmN809hPJsVMNGnKKRkNnEUG4Wog5js5y5I7ozdjRC0kfVnjAUbNYAVrb8CzK8DuK6JE6Gw5c92htgabR-46ZBwL1AN_MNv43w6UNKofQLHyw6uMbQRkpGTkEQ";
 
     cpr::Response r = cpr::Get(cpr::Url{ URI }, cpr::Bearer{ TOKEN });
@@ -42,6 +44,7 @@ int main(int argc, char** argv) {
     */
 
     cout << "Testing...." << std::endl;
+    //song and artist name
     string name = "hope";
     string artist = "meat computer";
     getTrackID(name, artist);
