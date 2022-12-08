5ea12e97eab7b1208dd2b7280da699120edf3570
lavde <104296090+pro0o@users.noreply.github.com>
2022-12-08 16:15:58 +0545

    Add files via upload

---
+++ genres.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include "tokenize_parse.h"
+
+using namespace std;
+
+string getgenres(string response);
+void Genre_Matchup();
---
+++ get_audiofeatures.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "tokenize_parse.h"
+#include <string>
+#include <vector>
+string get_audiofeatures(string response, string feature);
