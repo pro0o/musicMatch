288aefa8fe71354e982eb36f6760bd64fd02e0d1
lavde <104296090+pro0o@users.noreply.github.com>
2022-12-08 16:13:45 +0545

    Delete music_match_backend directory

--- music_match_backend/get_audiofeatures.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "tokenize_parse.h"
-#include <string>
-#include <vector>
-string get_audiofeatures(string response, string feature);
--- music_match_backend/get_trackid.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-
-#include "tokenize_parse.h"
-
-using namespace std;
-string get_trackid(string response);
